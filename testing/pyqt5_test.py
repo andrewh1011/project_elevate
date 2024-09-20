ab454163b5a802ab5d24e55954c431a4bfcb7b96
Andrew Hartmann <a.r.hartmann@wustl.edu>
2024-09-20 15:28:36 -0500

    learned pyqt5 program setup and UI interactivity

--- testing/pyqt5_test.py
+++ testing/pyqt5_test.py
@@ -2,18 +2,33 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class FirstWindow(QMainWindow):
+    def __init__(self):
+        super(FirstWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Project Elevate")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello World")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Add Data")
+        self.b1.clicked.connect(self.click_button)
+
+
+    def click_button(self):
+        self.label.setText("Button clicked")
+        self.update()
+    
+    def update(self):
+        print("change occured")
+
 def window():
     app = QApplication(sys.argv)
-
-    win = QMainWindow()
-    xpos = ypos = 200
-    width = height = 300
-    win.setGeometry(xpos, ypos, width, height)
-    win.setWindowTitle("Project Elevate")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("Hello World")
-
+    win = FirstWindow()
     win.show()
     sys.exit(app.exec_())
 
