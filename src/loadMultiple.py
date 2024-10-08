3327ab4475dded8b883e9890b6e2e1e13e8aedc2
Andrew Hartmann <a.r.hartmann@wustl.edu>
2024-10-08 15:41:12 -0500

    changed the way files are stored in dictionary to include which system they come from: :

--- src/loadMultiple.py
+++ src/loadMultiple.py
@@ -7,7 +7,7 @@ class Ui(QMainWindow):
 	def __init__(self):
 		super(Ui, self).__init__()
 
-		self.fileNames = {} # maps fileNames to fullFilePaths for when the user will need to load the actual file's date from the path.
+		self.fileNames = {} # maps fileNames to (fullFilePath, system) -> a tuple with important information about the file
 		self.ui = Ui_Dialog()
 		self.ui.setupUi(self)
 
@@ -29,11 +29,10 @@ class Ui(QMainWindow):
 		system = self.get_system()
 
 		if file and system:
-			print(system)
 			fileFullPath = file[0]
 			fileName = file[0].rsplit('/', 1)[-1]
 			self.ui.label.setText("File chosen: " + fileName)
-			self.add_to_list(fileName, fileFullPath)
+			self.add_to_list(fileName, fileFullPath, system)
 
 	def get_system(self):
 		systems = ["System 1", "System 2", "System 3", "System 4", "System 5", "System 6"]
@@ -46,15 +45,15 @@ class Ui(QMainWindow):
 		return None
 
 	def item_clicked(self, item):
-		self.ui.label.setText(self.fileNames[item.text()])
+		self.ui.label.setText(self.fileNames[item.text()][0])
 
-	def add_to_list(self,name,path):
+	def add_to_list(self, name, path, system):
 		
 		if name in self.fileNames:
 			self.ui.label.setText("DUPLICATE")
 			print(self.fileNames[name])
 		else:
-			self.fileNames[name] = path
+			self.fileNames[name] = (path, system)
 			itm = QListWidgetItem(name)
 			self.ui.listWidget.addItem(itm)
 
